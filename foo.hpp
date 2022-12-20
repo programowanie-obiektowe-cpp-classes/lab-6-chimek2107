15e1e7acfe22b119d15a794588e8b49fe3d0aba5
chimek2107 <b.chimkowski@tuta.io>
2022-12-20 21:38:28 +0100

    v1

--- foo.hpp
+++ foo.hpp
@@ -2,15 +2,21 @@
 
 #include "Human.hpp"
 
-#include <algorithm>
 #include <list>
 #include <vector>
+#include <iostream>
+#include <algorithm>
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
+	// Twoja implementacja tutaj
+	
+	std::vector< char > v(people.size());
 
-    // Twoja implementacja tutaj
+	std::for_each(people.begin(), people.end(), [](Human &n) { n.birthday(); });
+	std::transform(people.begin(), people.end(), v.begin(), [](const Human &n) { return n.isMonster() ? 'n' : 'y'; });
+	std::reverse(v.begin(), v.end());
 
-    return retval;
+
+	return v;
 }
